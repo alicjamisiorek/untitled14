c3407fa3b76b30cd21d5a7e8dfb10e860174f204
alicjamisiorek <58385065+alicjamisiorek@users.noreply.github.com>
2021-11-24 23:07:39 +0100

    Initial commit

---
+++ src/Projekt.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+
+public class Projekt {
+
+        public static void main(String[] args) {
+        /*Scanner scan = new Scanner(System.in);
+        System.out.println("podaj liczbe");
+        int liczba = scan.nextInt();*/
+            int[] liczby = { -5, 100, 1, 0, 20 };
+            for (int i = 0; i < liczby.length; i++) {
+                System.out.print(liczby[i] + " ");
+            }
+        }
+
+    }
+
+
